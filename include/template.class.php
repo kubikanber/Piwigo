0d5f2bc6af72c4a3f5a52d65574079ae595b274a
kubikanber <kubikanber@hotmail.com>
2023-01-14 17:05:22 +0300

    PHP Deprecated:  Using php-function "urlencode" as a modifier is deprecated and will be removed in a future release. Use Smarty::registerPlugin to explicitly register a custom modifier. in /opt/lampp/htdocs/piwigo/include/smarty/libs/sysplugins/smarty_internal_compile_private_modifier.php on line 112

--- include/template.class.php
+++ include/template.class.php
@@ -108,6 +108,7 @@ class Template
     $this->smarty->registerPlugin('modifier', 'explode', array('Template', 'mod_explode') );
     $this->smarty->registerPlugin('modifier', 'ternary', array('Template', 'mod_ternary') );
     $this->smarty->registerPlugin('modifier', 'get_extent', array($this, 'get_extent') );
+    $this->smarty->registerPlugin('modifier', 'urlencode', 'urlencode');  // TODO : add modifier , gelen uyarıya göre eklendi
     $this->smarty->registerPlugin('block', 'html_head', array($this, 'block_html_head') );
     $this->smarty->registerPlugin('block', 'html_style', array($this, 'block_html_style') );
     $this->smarty->registerPlugin('function', 'combine_script', array($this, 'func_combine_script') );
