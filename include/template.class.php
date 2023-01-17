fa5d2507340df0eabf22efa9cfaf0c93054537a4
kubikanber <kubikanber@hotmail.com>
2023-01-17 13:43:51 +0300

    PHP Deprecated:  Using php-function "ucfirst" as a modifier is deprecated and will be removed in a future release. Use Smarty::registerPlugin to explicitly register a custom modifier. in /opt/lampp/htdocs/piwigo/include/smarty/libs/sysplugins/smarty_internal_compile_private_modifier.php on line 112

--- include/template.class.php
+++ include/template.class.php
@@ -112,6 +112,7 @@ class Template
     $this->smarty->registerPlugin('modifier', 'intval', 'intval');  // TODO : add modifier , gelen uyarıya göre eklendi
     $this->smarty->registerPlugin('modifier','htmlspecialchars', 'htmlspecialchars');  // TODO : add modifier , gelen uyarıya göre eklendi
     $this->smarty->registerPlugin('modifier', 'sprintf', 'sprintf');  // TODO : add modifier , gelen uyarıya göre eklendi
+    $this->smarty->registerPlugin('modifier', 'ucfirst', 'ucfirst');  // TODO : add modifier , gelen uyarıya göre eklendi
     $this->smarty->registerPlugin('block', 'html_head', array($this, 'block_html_head') );
     $this->smarty->registerPlugin('block', 'html_style', array($this, 'block_html_style') );
     $this->smarty->registerPlugin('function', 'combine_script', array($this, 'func_combine_script') );
