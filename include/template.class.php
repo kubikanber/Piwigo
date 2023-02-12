146c549b3f27b9452758cea151dfe9a5253430ed
kubikanber <kubikanber@hotmail.com>
2023-02-13 00:03:44 +0300

    PHP Deprecated:  Using php-function "implode" as a modifier is deprecated and will be removed in a future release. Use Smarty::registerPlugin to explicitly register a custom modifier. in /opt/lampp/htdocs/piwigo/include/smarty/libs/sysplugins/smarty_internal_compile_private_modifier.php on line 112

--- include/template.class.php
+++ include/template.class.php
@@ -116,6 +116,7 @@ class Template
     $this->smarty->registerPlugin('modifier', 'constant', 'constant');  // TODO : add modifier , gelen uyarıya göre eklendi
     $this->smarty->registerPlugin('modifier', 'file_exists', 'file_exists');  // TODO : add modifier , gelen uyarıya göre eklendi
     $this->smarty->registerPlugin('modifier', 'json_encode', 'json_encode');  // TODO : add modifier , gelen uyariya göre
+    $this->smarty->registerPlugin('implode', 'implode', 'implode');  // TODO : add modifier , gelen uyariya göre
     $this->smarty->registerPlugin('block', 'html_head', array($this, 'block_html_head') );
     $this->smarty->registerPlugin('block', 'html_style', array($this, 'block_html_style') );
     $this->smarty->registerPlugin('function', 'combine_script', array($this, 'func_combine_script') );
