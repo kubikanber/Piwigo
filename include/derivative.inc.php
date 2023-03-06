673a32807a002c0c88d31c9b406678c000e60a71
kubikanber <kubikanber@hotmail.com>
2023-03-06 10:17:57 +0300

    PHP Warning: filemtime(): stat failed for include/derivative.inc.php on line 360

--- include/derivative.inc.php
+++ include/derivative.inc.php
@@ -357,7 +357,8 @@ final class DerivativeImage
     $url_style=$conf['derivative_url_style'];
     if (!$url_style)
     {
-      $mtime = @filemtime(PHPWG_ROOT_PATH.$rel_path);
+         // TODO: check is file exists ?? no than no E_WARNING is emitted.
+        $mtime = file_exists(PHPWG_ROOT_PATH.$rel_path) ? @filemtime(PHPWG_ROOT_PATH.$rel_path): false;
       if ($mtime===false or $mtime < $params->last_mod_time)
       {
         $is_cached = false;
